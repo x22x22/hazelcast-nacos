971ca52ef1346772f52111040b85a01a5779a57d
Kdump <rootshellexp@gmail.com>
2021-06-17 00:02:58 +0800

    调整单元测试
    调整单元测试

--- src/test/java/com/hazelcast/nacos/HazelcastIntegrationTest.java
+++ src/test/java/com/hazelcast/nacos/HazelcastIntegrationTest.java
@@ -40,7 +40,7 @@ public class HazelcastIntegrationTest {
         config.setProperty("hazelcast.discovery.enabled", "true");
 
         DiscoveryStrategyConfig discoveryStrategyConfig = new DiscoveryStrategyConfig(new NacosDiscoveryStrategyFactory());
-        discoveryStrategyConfig.addProperty(NacosDiscoveryProperties.SERVER_ADDR.key(), System.getenv("NACOS_REGISTRY_SERVER_ADDR"));
+        discoveryStrategyConfig.addProperty(NacosDiscoveryProperties.SERVER_ADDR.key(), System.getenv("NACOS_REGISTRY_SERVER_ADDR_TEST"));
         discoveryStrategyConfig.addProperty(NacosDiscoveryProperties.APPLICATION_NAME.key(), "hz-server");
         config.getNetworkConfig().getJoin().getDiscoveryConfig().addDiscoveryStrategyConfig(discoveryStrategyConfig);
 
@@ -53,28 +53,10 @@ public class HazelcastIntegrationTest {
         assertEquals(2, instance2Size);
     }
 
-    @SuppressWarnings({"unchecked"})
-    public static void removeEnv(String envName) throws ReflectiveOperationException {
-        Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
-        Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
-        theEnvironmentField.setAccessible(true);
-        Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
-        env.remove(envName);
-        Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
-        theCaseInsensitiveEnvironmentField.setAccessible(true);
-        Map<String, String> cienv = (Map<String, String>) theCaseInsensitiveEnvironmentField.get(null);
-        cienv.remove(envName);
-    }
-
     @Test
     public void testIntegration_urlNotConfigured() {
         exception.expect(IllegalStateException.class);
         exception.expectMessage("Nacos ServerAddr cannot be null.");
-        try {
-            removeEnv("NACOS_REGISTRY_SERVER_ADDR");
-        } catch (ReflectiveOperationException e) {
-            e.printStackTrace();
-        }
         Config config = new Config();
         config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
         config.setProperty("hazelcast.discovery.enabled", "true");
